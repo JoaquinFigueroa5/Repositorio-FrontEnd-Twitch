b1ceee95f40b5728f883a1fb76a7d8f72f19af4a
JoaquinFigueroa5 <kinchin.figueroa@gmail.com>
2025-04-22 16:45:17 -0600

    Channel View finished

--- src/shared/hooks/index.js
+++ src/shared/hooks/index.js
@@ -3,4 +3,6 @@ export { useRegister } from "./useRegister";
 export { logout } from "./userLogout";
 export { useUserDetails } from "./useUserDetails";
 export { useChangePassword } from "./useChangePassword";
-export { useChannelSetting } from "./useChannelSettings"
+export { useChannelSetting } from "./useChannelSettings";
+export { useChannelDetails } from "./useChannelDetails";
+export { useFollowChannel } from "./useFollowChannel";
